716e41886948e6db8e05f4d18895959a3be2b331
Will Dewhurst <will.dewhurst@ans.co.uk>
2022-12-08 02:49:23 +0000

    BUG: Cleaning up unused example

--- app/fresh.gen.ts
+++ app/fresh.gen.ts
@@ -5,14 +5,13 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/api/nasa/apod.ts";
+import * as $2 from "./routes/api/nasa/apod.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_500.tsx": $1,
-    "./routes/api/nasa/apod.ts": $3,
+    "./routes/api/nasa/apod.ts": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
